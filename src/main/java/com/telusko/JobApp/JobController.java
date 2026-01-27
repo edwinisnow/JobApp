830aea56c82330497e2537f7476dc9bee49f247a
EDWIN GEORGE LAZER <edwin04@gmail.com>
2026-01-27 12:02:15 +0000

    Handling form

--- src/main/java/com/telusko/JobApp/JobController.java
+++ src/main/java/com/telusko/JobApp/JobController.java
@@ -1,22 +1,24 @@
 package com.telusko.JobApp;
 
+import com.telusko.JobApp.model.JobPost;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
-    @RequestMapping({"/", "home"})
+    @GetMapping({"/", "home"})
     public String home() {
         return "home";
     }
 
-    @RequestMapping("addjob")
+    @GetMapping("addjob")
     public String addjob() {
         return "addjob";
     }
 
-    @RequestMapping("handleForm")
-    public String handleForm() {
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost) {
         return "success";
     }
 }
